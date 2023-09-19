dd22ea13378e4d8e0ca23c600f2c08b45a08c43d
Ruan6400 <133367215+Ruan6400@users.noreply.github.com>
2023-09-19 10:25:51 -0300

    Update ListaLigadaOrdenada.cpp
    ajuste de tabulação

--- ListaLigada/ListaLigadaOrdenada/ListaLigadaOrdenada.cpp
+++ ListaLigada/ListaLigadaOrdenada/ListaLigadaOrdenada.cpp
@@ -1,43 +1,43 @@
 #include <iostream>       
-  #include <cmath>           
-  #include <cstdio>             
-  using namespace std;             
+#include <cmath>           
+#include <cstdio>             
+using namespace std;             
   
-  // definicao de tipo             
-  struct NO {             
-     int valor;             
-     NO* prox;             
-  };             
+// definicao de tipo             
+struct NO {             
+    int valor;             
+    NO* prox;             
+};             
   
-  NO* primeiro = NULL;             
-  NO* MeioDaLista = NULL;          
-  int quantidade = 0;          
-  int Soma = 0;          
-  float Media;        
-  int MediaInt;        
+NO* primeiro = NULL;             
+NO* MeioDaLista = NULL;          
+int quantidade = 0;          
+int Soma = 0;          
+float Media;        
+int MediaInt;        
   
-  // headers             
-  void menu();             
-  void inicializar();             
-  void exibirQuantidadeElementos();             
-  void exibirElementos();             
-  void inserirElemento();             
-  void excluirElemento();             
-  void buscarElemento();     
-  void Apagador(NO* ValorInicial,int NumExc, bool Tanomeio);     
-  void MostrarDados();  
-  void Procurar(int Num,NO* Inicio,NO* Fim);  
-  NO* posicaoElemento(int numero);             
-             //--------------------------             
+// headers             
+void menu();             
+void inicializar();             
+void exibirQuantidadeElementos();             
+void exibirElementos();             
+void inserirElemento();             
+void excluirElemento();             
+void buscarElemento();     
+void Apagador(NO* ValorInicial,int NumExc, bool Tanomeio);     
+void MostrarDados();  
+void Procurar(int Num,NO* Inicio,NO* Fim);  
+NO* posicaoElemento(int numero);             
+//--------------------------             
   
   
-  int main(){             
+int main(){             
     menu();             
-  }             
+}             
   
-  void menu(){             
-     int op = 0;             
-     while (op != 7) {             
+void menu(){             
+    int op = 0;             
+    while (op != 7) {             
         system("clear"); // somente no windows             
         cout << "Menu Lista Ligada";             
         cout << endl << endl;             
@@ -71,276 +71,273 @@
            default: break;             
         }             
         getchar();             
-     }             
-  }             
+    }             
+}             
   
   
-  void inicializar(){             
-     // se a lista já possuir elementos             
-     // libera a memoria ocupada             
-     NO* aux = primeiro;             
-     while (aux != NULL) {             
+void inicializar(){             
+    // se a lista já possuir elementos             
+    // libera a memoria ocupada             
+    NO* aux = primeiro;             
+    while (aux != NULL) {             
         NO* paraExcluir = aux;             
         aux = aux->prox;             
         free(paraExcluir);             
-     }             
+    }             
   
-     primeiro = NULL;     
-     quantidade = 0;    
-     cout << "Lista inicializada \n";             
-     cout << "\nPressione ENTER para continuar\n";             
-     getchar();             
+    primeiro = NULL;     
+    quantidade = 0;    
+    cout << "Lista inicializada \n";             
+    cout << "\nPressione ENTER para continuar\n";             
+    getchar();             
   
-  }             
+}             
   
-  void exibirQuantidadeElementos() {             
+void exibirQuantidadeElementos() {             
   
-     int nElementos = 0;             
-     NO* aux = primeiro;             
-     while (aux != NULL) {             
+    int nElementos = 0;             
+    NO* aux = primeiro;             
+    while (aux != NULL) {             
         nElementos++;             
         aux = aux->prox;             
-     }             
-     cout << "Quantidade de elementos: " << nElementos << endl;     
-     cout << "\nPressione ENTER para continuar\n";             
-     getchar();     
+    }             
+    cout << "Quantidade de elementos: " << nElementos << endl;     
+    cout << "\nPressione ENTER para continuar\n";             
+    getchar();     
   
-  }             
+}             
   
-  void exibirElementos(){             
-     if(primeiro == NULL) {             
+void exibirElementos(){             
+    if(primeiro == NULL) {             
         cout << "Lista vazia \n";             
         cout << "\nPressione ENTER para continuar\n";             
         getchar();             
         return;             
-     }             
-     else {             
+    }             
+    else {             
         cout << "Elementos: \n";             
         NO* aux = primeiro;             
         while (aux != NULL){             
-           cout << aux->valor << endl;             
-           aux = aux->prox;             
+            cout << aux->valor << endl;             
+            aux = aux->prox;             
         }             
         cout << "\nPressione ENTER para continuar\n";             
         getchar();             
      }             
   }             
   
- void inserirElemento(){ 
+void inserirElemento(){ 
     int ValNum;             
     cout << "Digite o elemento: ";             
     cin >> ValNum;             
     bool Inseriu=false;             
     // aloca memoria dinamicamente para o novo elemento             
     if(posicaoElemento(ValNum)==NULL){             
-  
-       NO* novo = (NO*)malloc(sizeof(NO));             
-       bool PrimeiraVez=true;             
-       if (novo == NULL){             
-           return;             
-       }             
-       novo->valor=ValNum;             
-       novo->prox = NULL;             
-       NO* ValorAtual;             
-       NO* Anterior;
-       NO* Final;
-       if(quantidade>=5){
-           if(novo->valor<MeioDaLista->valor){
-               ValorAtual=MeioDaLista;
-               Final=NULL;
-           }
-           else{
-               ValorAtual=primeiro;
-               Final=MeioDaLista->prox;
-           }
-       }
-       else{
-           ValorAtual=primeiro;
-           Final=NULL;
-       }
+        NO* novo = (NO*)malloc(sizeof(NO));             
+        bool PrimeiraVez=true;             
+        if (novo == NULL){             
+            return;             
+        }             
+        novo->valor=ValNum;             
+        novo->prox = NULL;             
+        NO* ValorAtual;             
+        NO* Anterior;
+        NO* Final;
+        if(quantidade>=5){
+            if(novo->valor<MeioDaLista->valor){
+                ValorAtual=MeioDaLista;
+                Final=NULL;
+            }
+            else{
+                ValorAtual=primeiro;
+                Final=MeioDaLista->prox;
+            }
+        }
+        else{
+            ValorAtual=primeiro;
+            Final=NULL;
+        }
        
-       if(primeiro==NULL){          
-           primeiro=novo;
-       }          
+        if(primeiro==NULL){          
+            primeiro=novo;
+        }          
         else{          
-           while(ValorAtual!=Final){
-               if(novo->valor>ValorAtual->valor){
-                   novo->prox=ValorAtual;
-                   primeiro=novo;
-                   break;
-               }
-               else{
-                   if(ValorAtual->prox==Final||novo->valor>ValorAtual->prox->valor){
-                       novo->prox=ValorAtual->prox;
-                       ValorAtual->prox=novo;
-                       break;
-                   }
-                   ValorAtual=ValorAtual->prox;
-               }
-           }          
+            while(ValorAtual!=Final){
+                if(novo->valor>ValorAtual->valor){
+                    novo->prox=ValorAtual;
+                    primeiro=novo;
+                    break;
+                }
+                else{
+                    if(ValorAtual->prox==Final||novo->valor>ValorAtual->prox->valor){
+                        novo->prox=ValorAtual->prox;
+                        ValorAtual->prox=novo;
+                        break;
+                    }
+                    ValorAtual=ValorAtual->prox;
+                }
+            }          
         }          
         cout <<"Valor inserido \n";           
         quantidade++; 
         MediaInt = (quantidade - (quantidade % 2))/2; 
         ValorAtual=primeiro; 
         for(int i=0;i<quantidade+1;i++){    
-           if(i==MediaInt){    
-              MeioDaLista=ValorAtual; 
-              break; 
-           }    
-           else{    
-              ValorAtual=ValorAtual->prox;    
-           }    
+            if(i==MediaInt){    
+                MeioDaLista=ValorAtual; 
+                break; 
+            }    
+            else{    
+                ValorAtual=ValorAtual->prox;    
+            }    
         } 
         cout << "\nPressione ENTER para continuar\n";             
         getchar();             
-     }             
-     else{             
+    }             
+    else{             
         cout << "Esse valor ja esta na lista";             
         cout << "\nPressione ENTER para continuar\n";             
         getchar();             
-     }             
+    }             
   
- }         
+}         
   
-             void excluirElemento(){         
-                 int NumExc;       
-                 NO* ValorAtual=primeiro;       
-                 NO* Anterior=primeiro;       
-                 if(quantidade<=0){         
-                     cout<<"Lista Vazia.";         
-                     getchar();         
-                 }         
-                 else{         
-                     cout<<"Digite o numero a ser excluido:\n";         
-                     cin >> NumExc;         
-                     if(quantidade>=3&&MeioDaLista!=NULL){        
-                         if(NumExc<MeioDaLista->valor){       
-                             cout<<"contando do meio da lista"<<endl;       
-                             Apagador(MeioDaLista,NumExc,true);       
-                         }        
-                         else{       
-                             cout<<"contando do comeco da lista";       
-                             Apagador(primeiro,NumExc,false);        
-                         }        
-                     }         
-                     else{         
-                         Apagador(primeiro,NumExc,false);         
-                     }       
-                     MediaInt = (quantidade - (quantidade % 2))/2; 
-                     ValorAtual=primeiro;  
-                     for(int i=0;i<quantidade+1;i++){    
-                         if(i==MediaInt){    
-                             MeioDaLista=ValorAtual; 
-                             break; 
-                         }    
-                         else{    
-                             ValorAtual=ValorAtual->prox;    
-                         }    
-                     }     
-                 }         
-             }         
+void excluirElemento(){         
+    int NumExc;       
+    NO* ValorAtual=primeiro;       
+    NO* Anterior=primeiro;       
+    if(quantidade<=0){         
+        cout<<"Lista Vazia.";         
+        getchar();         
+    }         
+    else{         
+        cout<<"Digite o numero a ser excluido:\n";         
+        cin >> NumExc;         
+        if(quantidade>=3&&MeioDaLista!=NULL){        
+            if(NumExc<MeioDaLista->valor){       
+                cout<<"contando do meio da lista"<<endl;       
+                Apagador(MeioDaLista,NumExc,true);       
+            }        
+            else{       
+                cout<<"contando do comeco da lista";       
+                Apagador(primeiro,NumExc,false);        
+            }        
+        }         
+        else{         
+            Apagador(primeiro,NumExc,false);         
+        }       
+            MediaInt = (quantidade - (quantidade % 2))/2; 
+            ValorAtual=primeiro;  
+            for(int i=0;i<quantidade+1;i++){    
+                if(i==MediaInt){    
+                    MeioDaLista=ValorAtual; 
+                    break; 
+                }    
+                else{    
+                    ValorAtual=ValorAtual->prox;    
+                }    
+            }     
+    }         
+}         
   
   
   
-             void buscarElemento(){  
-                 int NumBusca;  
-                 if(quantidade<=0){  
-                     cout<<"Lista Vazia. \n";  
-                     getchar();  
-                 }  
-                 else{  
-                     cout<<"Digite o valor: \n";  
-                     cin>>NumBusca;  
-                     if(quantidade>=5){  
-                         if(NumBusca<MeioDaLista->valor){  
-                              Procurar(NumBusca,MeioDaLista,NULL);  
-                         }  
-                         else{  
-                              Procurar(NumBusca,primeiro,MeioDaLista);  
-                         }  
-                     }  
-                     else{  
-                         Procurar(NumBusca,primeiro,NULL);  
-                     }  
-                 }  
-             }       
+void buscarElemento(){  
+    int NumBusca;  
+    if(quantidade<=0){  
+        cout<<"Lista Vazia. \n";  
+        getchar();  
+    }  
+    else{  
+        cout<<"Digite o valor: \n";  
+        cin>>NumBusca;  
+        if(quantidade>=5){  
+            if(NumBusca<MeioDaLista->valor){  
+                Procurar(NumBusca,MeioDaLista,NULL);  
+            }  
+            else{  
+                Procurar(NumBusca,primeiro,MeioDaLista);  
+            }  
+        }  
+        else{  
+            Procurar(NumBusca,primeiro,NULL);  
+        }  
+    }  
+}       
   
-             void MostrarDados(){       
-                 cout<<"MediaInt: "<<MediaInt<<endl;       
-                 cout<<"quant: "<<quantidade<<endl;       
-                 if(MeioDaLista==NULL){       
-                     cout<<"a variavel esta nula";       
-                 }       
-                 else{       
-                     cout<<"MeioDaLista: "<<MeioDaLista->valor;   
+void MostrarDados(){       
+    cout<<"MediaInt: "<<MediaInt<<endl;       
+    cout<<"quant: "<<quantidade<<endl;       
+    if(MeioDaLista==NULL){       
+        cout<<"a variavel esta nula";       
+    }       
+    else{       
+        cout<<"MeioDaLista: "<<MeioDaLista->valor;   
+    }       
+    getchar();       
+}       
   
-                 }       
-                 getchar();       
-             }       
+NO* posicaoElemento(int numero/*,NO* ValorInicial*/)  {               
+    NO* aux = primeiro; //mudar valor de aux para o argumento               
+    while (aux != NULL) {               
+        if (aux->valor == numero){               
+            break;               
+        }               
+        aux = aux->prox;               
+    }               
+    return aux;               
+}  
   
-             NO* posicaoElemento(int numero/*,NO* ValorInicial*/)  {               
-                       NO* aux = primeiro; //mudar valor de aux para o argumento               
-                       while (aux != NULL) {               
-                               if (aux->valor == numero)               
-                               {               
-                                       break;               
-                               }               
-                               aux = aux->prox;               
-                       }               
-                       return aux;               
-             }  
+void Procurar(int Num,NO* Inicio,NO* Fim){  
+    while(Inicio!=Fim){  
+        if(Inicio->valor==Num){  
+            cout<<"\nValor encontrado\n";  
+            break;  
+        }  
+        else{  
+            if(Inicio->prox==Fim){  
+                cout<<"\nValor nao encontrado\n";  
+                break;  
+            }  
+        }  
+        Inicio=Inicio->prox;  
+    }  
+    getchar();  
+}  
   
-             void Procurar(int Num,NO* Inicio,NO* Fim){  
-                 while(Inicio!=Fim){  
-                     if(Inicio->valor==Num){  
-                         cout<<"\nValor encontrado\n";  
-                         break;  
-                     }  
-                     else{  
-                         if(Inicio->prox==Fim){  
-                             cout<<"\nValor nao encontrado\n";  
-                             break;  
-                         }  
-                     }  
-                     Inicio=Inicio->prox;  
-                 }  
-                 getchar();  
-             }  
-  
-             void Apagador(NO* ValorInicial,int NumExc, bool Tanomeio){         
-                 NO* ValorAtual;         
-                 NO* Anterior;
-                 NO* Fim;
-                 if(Tanomeio){     
-                     ValorAtual=ValorInicial->prox;     
-                     Anterior=ValorInicial;
-                     Fim=NULL;
-                 }     
-                 else{     
-                     ValorAtual=ValorInicial;
-                     Fim=MeioDaLista;
-                 }
-                 while(ValorAtual!=Fim){
-                     if(ValorAtual->valor==NumExc){
-                         if(ValorAtual->valor==primeiro->valor){
-                           primeiro=ValorAtual->prox;
-                         }
-                         else{
-                           Anterior->prox=ValorAtual->prox;
-                         }
-                         free(ValorAtual);
-                         cout<<"\nValor Excluido.\n";
-                         break;
-                         quantidade--;
-                     }
-                     else{
-                         if(ValorAtual->prox==Fim){
-                             cout<<"\nValor nao encontrado.\n";
-                         }
-                         Anterior=ValorAtual;
-                         ValorAtual=ValorAtual->prox;
-                     }
-                 }         
-                 getchar();         
-             }
+void Apagador(NO* ValorInicial,int NumExc, bool Tanomeio){         
+    NO* ValorAtual;         
+    NO* Anterior;
+    NO* Fim;
+    if(Tanomeio){     
+        ValorAtual=ValorInicial->prox;     
+        Anterior=ValorInicial;
+        Fim=NULL;
+    }     
+    else{     
+        ValorAtual=ValorInicial;
+        Fim=MeioDaLista;
+    }
+    while(ValorAtual!=Fim){
+        if(ValorAtual->valor==NumExc){
+            if(ValorAtual->valor==primeiro->valor){
+                primeiro=ValorAtual->prox;
+            }
+            else{
+                Anterior->prox=ValorAtual->prox;
+            }
+            free(ValorAtual);
+            cout<<"\nValor Excluido.\n";
+            break;
+            quantidade--;
+        }
+        else{
+            if(ValorAtual->prox==Fim){
+                cout<<"\nValor nao encontrado.\n";
+            }
+            Anterior=ValorAtual;
+            ValorAtual=ValorAtual->prox;
+        }
+    }         
+    getchar();         
+}
